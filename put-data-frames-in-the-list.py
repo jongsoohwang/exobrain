4b3aa1d633902df0d254883df15989743be5004f
JongSoo Hwang <jongsoo.data@gmail.com>
2021-01-07 09:57:54 +0900

    Create put-data-frames-in-the-list.py

---
+++ put-data-frames-in-the-list.py
@@ -0,0 +1,14 @@
+###########
+# example #
+###########
+
+import pandas as pd
+
+a_list_of_dataframes = []
+
+# for level in range(100):
+#   ## 여러 조건들 추가
+#   # df = pd.Dataframe(...)
+#   
+#   df = pd.Dataframe()
+#   a_list_of_dataframes.append(df)
